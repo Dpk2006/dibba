de1fe99fc1d402d763fd11c4c6b51491944e5e42
jaat <jaat@gmail.com>
2025-09-13 19:06:36 +0530

    test

--- server/server.js
+++ server/server.js
@@ -8,9 +8,6 @@ const PORT = process.env.PORT || 3000;
 
 connectMongo();
 
-app.get("/",(req,res)=>{
-    res.send(true);
-})
 
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`)
